afc35ddb01860028f229ef56143ea875fb15aa63
Nikki <nikki.satopdx@gmail.com>
2020-03-16 13:02:12 -0700

    making some changing to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './footer.scss';
 
 const About = () => (
-  <footer className='footer'>2020 Nikki Sato - Made with React </footer>
+  <footer>
+    <div className='footer'>2020 Nikki Sato - Made with React</div>
+  </footer>
 );
 
 export default About;
